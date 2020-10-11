997eadca434c14ec3f326e37fc836fdc925d9947
forsythrl <tarrantrl@utexas.edu>
2020-10-10 21:44:53 -0500

    add post route

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const { animals } = require('./data/animals.json');
 
@@ -6,6 +8,10 @@ const PORT = process.env.PORT || 3001;
 
 // instantiate server
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({extended: true}));
+// parse incoming json data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -46,6 +52,32 @@ function findById(id, animalsArray){
     return result;
 }
 
+function createNewAnimal(body, animalsArray){
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({animals: animalsArray}, null, 2)
+    )
+    return animal;
+}
+
+function validateAnimal(animal){
+    if (!animal.name || typeof animal.name !== 'string'){
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string'){
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string'){
+        return false;
+    }
+    if (!animal.personalityTraits || !(Array.isArray(animal.personalityTraits))) {
+        return false;
+      }
+    return true;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -54,6 +86,7 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 })
 
+// params come immediately after the animals/, unlike query string which comes after ?
 app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
     if (result){
@@ -61,6 +94,19 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
         res.send(404);
     }
+})
+
+app.post('/api/animals', (req, res) => {
+    // req.body is incoming content
+    // set id for incoming data based on length of existing data array
+    req.body.id = animals.length.toString();
+    if (!validateAnimal(req.body)){
+        // 400 indicates user error
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }  
 })
 
 // set up server to listen on 3001
